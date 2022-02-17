59faa883bbc43ae842c170bb3cca8a5d3f89eeac
ArtisticZhao <hkcreedblacktea@hotmail.com>
2022-02-17 08:39:16 +0800

    - 修正了ui文件提升类包含路径的问题 - 优化重命名的流程

--- SqlQueryModel.cpp
+++ SqlQueryModel.cpp
@@ -26,9 +26,13 @@ bool SqlQueryModel::setData(const QModelIndex& index, const QVariant& value, int
 	}
 	// 修改name之后的操作：更新文件名，更新数据库中对应的path
 	if (index.column() == static_cast<int>(MovieTableColumIndex::Name)) {
-		// 1. 检查更改后的文件名是否符合文件名规则
-		//auto namestr = this->data(this->index(index.row(), static_cast<int>(MovieTableColumIndex::Name))).toString();
+		auto oldNameStr = this->data(this->index(index.row(), static_cast<int>(MovieTableColumIndex::Name))).toString();
 		auto name = value.toString();
+		if (QString::compare(oldNameStr, name) == 0) {
+			// 没有发生变更
+			return false;
+		}
+		// 1. 检查更改后的文件名是否符合文件名规则
 		bool islegal = FileOperator::isLegalFileName(name);
 		if (!islegal) {
 			return false;
@@ -36,7 +40,6 @@ bool SqlQueryModel::setData(const QModelIndex& index, const QVariant& value, int
 		// 2. 修改文件名
 		auto fullpath = this->data(this->index(index.row(), static_cast<int>(MovieTableColumIndex::Path))).toString();
 		auto newpath = FileOperator::renameFile(fullpath, name);
-		qDebug() << newpath;
 		// 3. 修改对应数据库中path路径
 		auto movieid = this->data(this->index(index.row(), static_cast<int>(MovieTableColumIndex::ID))).toInt();
 		emit movieNamePathUpdate(movieid, name, newpath);
